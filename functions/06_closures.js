571b4489f1a460fdd071287a13a487c6ff524836
fdAssis <franciscodeassisc.santos@gmail.com>
2021-12-15 00:01:35 -0300

    closures function

--- functions/06_closures.js
+++ functions/06_closures.js
@@ -1,36 +1,48 @@
 /**
- * Closures -> Escopo que é criado quando uma função é declarada
- * dado que esse escopo permite a função acessar e manipular dados 
- * declarados externos a função. 
+ * Se uma variável não pôde ser encontrada no escopo imediato, 
+ *  a engine irá procurar por ela no escopo externo mais próximo. 
+ *  Essa busca irá continuar até que o escopo global seja alcançado. 
+ *  Se a variável não estiver no escopo global, um erro será emitido.
  */
 
-let value = 'global';
+let value = 0;
 
-function myPrint(){
-  console.log(value);
+function simple_sum(){
+  return value += 1;
 }
 
 function other(){
-  let value = 'local';
-  myPrint();
-  console.log(value);
+  let value = 0;
+  simple_sum();
+  return value += 1;
 }
 
-other();
+console.log(`No closures 1: ${other()}`);
+console.log(`No closures 2: ${other()}`);
+console.log(`No closures 3: ${other()}`);
 
-/** ******************* */
+console.log("\n***********************\n")
+/**
+ * Closures -> Escopo que é criado quando uma função é declarada
+ *  dado que esse escopo permite a função acessar e manipular dados 
+ *  declarados externos a função. 
+ * 
+ * Closure é a forma de fazer com que as variáveis dentro de uma função 
+ *  sejam privadas e persistentes.
+ */
 
-let variable = 'global'
-
+ let variable = 0
 function myExternalFunction(){
-  let variable = 'local';
+  let variable = 0;
 
   function internal(){
-    return variable;
+    return variable += 1;
   }
 
   return internal;
 }
 
 let exe = myExternalFunction();
-console.log(exe());
+console.log(`With closures 1: ${exe()}`);
+console.log(`With closures 2: ${exe()}`);
+console.log(`With closures 3: ${exe()}`);

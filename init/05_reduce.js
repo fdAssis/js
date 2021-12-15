408291a598a41b4eba7ea2da9a968ee7151f0e1b
fdAssis <franciscodeassisc.santos@gmail.com>
2021-12-14 22:43:50 -0300

    summary with reduce

--- init/05_reduce.js
+++ init/05_reduce.js
@@ -14,10 +14,34 @@ res = array_numbers.reduce(sum);
  * 3 => 80 + 15 = 95
  */
 
-console.log(res)
+// console.log(res)
 
 res = array_numbers.filter(value => value > 15)
             .map(value => value * 0.5)
             .reduce((a, b) => a + b)
 
-console.log(res);
+// console.log(res);
+
+const transactions = [
+  {"id":"01", "type":"deposit", "amount": 100},
+  {"id":"02", "type":"withdraw", "amount": 200},
+  {"id":"03", "type":"withdraw", "amount": 50},
+  {"id":"04", "type":"deposit", "amount": 200}
+]
+
+const summary = transactions.reduce((acc, transaction) => {
+  if(transaction.type === 'deposit'){
+    acc.deposit += transaction.amount
+    acc.total += transaction.amount
+  } else {
+    acc.withdraw += transaction.amount
+    acc.total -= transaction.amount
+  }
+  return acc;
+}, {
+  deposit: 0,
+  withdraw: 0,
+  total: 0
+})
+
+console.log(summary)

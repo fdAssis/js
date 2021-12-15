d34499140d3baa7ff83becaf66ba1d629c8c204a
fdAssis <franciscodeassisc.santos@gmail.com>
2021-12-14 22:02:50 -0300

    init/map

--- init/03_map.js
+++ init/03_map.js
@@ -1,18 +1,18 @@
-let number = [2, 4, 6, 8, 10];
+let numbers = [2, 4, 6, 8, 10];
 
-var mult_2 = number.map(function(value) {
+var mult_2 = numbers.map(function(value) {
   return value * 2;
 })
 console.log(mult_2)
 
-var mutl_3 = number.map(value => {
+var mutl_3 = numbers.map(value => {
   return value * 3;
 })
 
 
 console.log(mutl_3)
 
-var mutl_4 = number.map(value => (value * 4))
+var mutl_4 = numbers.map(value => (value * 4))
 console.log(mutl_4)
 
 function func_1(value){
@@ -27,5 +27,5 @@ function func_3(value){
   return value / 5;
 }
 
-var result = number.map(func_1).map(func_2).map(func_3);
+var result = numbers.map(func_1).map(func_2).map(func_3);
 console.log(result);

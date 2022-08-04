68d490349d079b11da6cc223f5a4fd7591a3678a
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-08-03 22:01:45 -0300

    update 06_reduce.js

--- basic/05_reduce.js
+++ basic/05_reduce.js
@@ -1,4 +1,4 @@
-let array_numbers = [10, 30, 40, 15]
+let array_numbers = [10, 30, 40, 15];
 
 let res = 0;
 
@@ -16,32 +16,38 @@ res = array_numbers.reduce(sum);
 
 // console.log(res)
 
-res = array_numbers.filter(value => value > 15)
-            .map(value => value * 0.5)
-            .reduce((a, b) => a + b)
+res = array_numbers
+  .filter((value) => value > 15)
+  .map((value) => value * 0.5)
+  .reduce((a, b) => a + b);
 
 // console.log(res);
 
 const transactions = [
-  {"id":"01", "type":"deposit", "amount": 100},
-  {"id":"02", "type":"withdraw", "amount": 200},
-  {"id":"03", "type":"withdraw", "amount": 50},
-  {"id":"04", "type":"deposit", "amount": 200}
-]
+  { id: "01", type: "deposit", amount: 100 },
+  { id: "02", type: "withdraw", amount: 200 },
+  { id: "03", type: "withdraw", amount: 50 },
+  { id: "04", type: "deposit", amount: 200 },
+];
 
-const summary = transactions.reduce((acc, transaction) => {
-  if(transaction.type === 'deposit'){
-    acc.deposit += transaction.amount
-    acc.total += transaction.amount
-  } else {
-    acc.withdraw += transaction.amount
-    acc.total -= transaction.amount
+const summary = transactions.reduce(
+  (acc, transaction) => {
+    if (transaction.type === "deposit") {
+      acc.deposit += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraw += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+    return acc;
+  },
+  {
+    deposit: 0,
+    withdraw: 0,
+    total: 0,
   }
-  return acc;
-}, {
-  deposit: 0,
-  withdraw: 0,
-  total: 0
-})
+);
 
-console.log(summary)
+console.log(summary);
+
+[10, 20, 30, 40, 50].reduce(function (acc, current) {}, []);

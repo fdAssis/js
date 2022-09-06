73978b49706d6e6f27784c7620ff6b83dec31902
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-09-05 22:19:35 -0300

    refactor: add constructor function example

--- functions/05_constructor_function.js
+++ functions/05_constructor_function.js
@@ -1,6 +1,6 @@
 // Criando uma funcao construtora
 
-function Person(n, g, race = 'human') {
+function Person(n, g, race = "human") {
   // atributos privados acesso permitido somente dentro da funcao
   let height = 0;
   let weight = 0;
@@ -33,8 +33,29 @@ function Person(n, g, race = 'human') {
   };
 }
 
-const agelina = new Person('Agelina', 'Femenino');
+const agelina = new Person("Agelina", "Femenino");
 agelina.birday();
 
 console.log(typeof Person);
 console.log(typeof agelina);
+
+/**
+ * #####################################################################
+ */
+
+function User(name, country) {
+  // this = {}
+  this.name = name;
+  this.country = country;
+
+  this.speak = function () {
+    console.log(`Hy! my name is ${this.name}`);
+  };
+
+  // return this;
+}
+
+const user_1 = new User("Jão", "Brazil");
+console.group("User");
+user_1.speak();
+console.groupEnd("User");
